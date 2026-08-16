6b67479ae5429df209944399d1becf4436cbd306
Aliaksei Baranau <baralex05@gmail.com>
2026-08-16 13:47:52 +0200

    add ROC curve and confusion matrix evaluation plots

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, train_test_split
 from xgboost import XGBClassifier
 from src.pipeline import build_pipeline
+from src.evaluate import plot_confusion_matrix, plot_roc_curves
 
 
 PATH_TO_CSV_FILE = "data/raw/Titanic-Dataset.csv"
@@ -50,7 +51,7 @@ def train_and_evaluate(
         X_train: pd.DataFrame,
         y_train: pd.Series,
 ) -> Pipeline:
-    best_accuracy = 0.0
+    best_auc = 0.0
     best_pipeline = None
 
     for name, model in models.items():
@@ -59,11 +60,12 @@ def train_and_evaluate(
         # use cross validation to get more accurate results
         # divide data into 5 folds and have 5 iterations
         # so every fold can be a validating set
-        scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring="accuracy")
-        print(f"{name}: {scores.mean():.4f} ± {scores.std():.4f}")
+        acc_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring="accuracy")
+        auc_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring="roc_auc")
+        print(f"{name} | acc: {acc_scores.mean():.4f} ± {acc_scores.std():.4f} | auc: {auc_scores.mean():.4f} ± {auc_scores.std():.4f}")
 
-        if scores.mean() > best_accuracy:
-            best_accuracy = scores.mean()
+        if auc_scores.mean() > best_auc:
+            best_auc = auc_scores.mean()
             best_pipeline = pipeline
             
     return best_pipeline
@@ -80,5 +82,8 @@ if __name__ == "__main__":
     best_pipeline.fit(X_train, y_train)
     final_score = best_pipeline.score(X_val, y_val)
     print(f"Final score: {final_score:.4f}")
+
+    plot_roc_curves(models=models, X_train=X_train, X_val=X_val, y_train=y_train, y_val=y_val)
+    plot_confusion_matrix(best_pipeline=best_pipeline, X_val=X_val, y_val=y_val)
 
     save_model(model=best_pipeline, filepath=PATH_TO_BEST_MODEL)
